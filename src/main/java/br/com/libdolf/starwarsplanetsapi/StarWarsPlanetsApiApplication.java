7c792539071bc446681ab7d00b1c5c25440713e7
lindolfo <lindolfomoizinho1@gmail.com>
2024-02-25 20:07:27 -0300

    Initial commit

---
+++ src/main/java/br/com/libdolf/starwarsplanetsapi/StarWarsPlanetsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.libdolf.starwarsplanetsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsPlanetsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsPlanetsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/libdolf/starwarsplanetsapi/StarWarsPlanetsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.libdolf.starwarsplanetsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsPlanetsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

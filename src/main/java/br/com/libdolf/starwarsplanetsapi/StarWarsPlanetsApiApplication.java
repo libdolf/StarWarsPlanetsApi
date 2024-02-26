f8dd73f76781a1e46a4a1e5577003eb4bfbf2fd4
lindolfo <lindolfomoizinho1@gmail.com>
2024-02-25 21:20:47 -0300

    Configure swagger

--- src/main/java/br/com/libdolf/starwarsplanetsapi/StarWarsPlanetsApiApplication.java
+++ src/main/java/br/com/libdolf/starwarsplanetsapi/StarWarsPlanetsApiApplication.java
@@ -1,9 +1,12 @@
 package br.com.libdolf.starwarsplanetsapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Swagger OpenAPI", version = "1", description = "Teste"))
 public class StarWarsPlanetsApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/libdolf/starwarsplanetsapi/controller/SWPController.java
+++ src/main/java/br/com/libdolf/starwarsplanetsapi/controller/SWPController.java
@@ -3,6 +3,9 @@ package br.com.libdolf.starwarsplanetsapi.controller;
 import br.com.libdolf.starwarsplanetsapi.dtos.PlanetRequest;
 import br.com.libdolf.starwarsplanetsapi.dtos.PlanetResponse;
 import br.com.libdolf.starwarsplanetsapi.service.SWPService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -16,16 +19,30 @@ import java.util.List;
 public class SWPController {
     private SWPService service;
 
+    @Operation(summary = "Salva um novo planeta", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Salvo com sucesso!")
+    })
     @PostMapping()
     public ResponseEntity<PlanetResponse> postNewPlanet(@RequestBody PlanetRequest planet) throws Exception {
         return ResponseEntity.ok(service.saveNewPlanet(planet));
     }
 
+    @Operation(summary = "Lista todos os Planetas", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Todos os planetas")
+    })
     @GetMapping("/all")
     public ResponseEntity<List<PlanetResponse>> getAllPlanets(){
         return ResponseEntity.ok(service.findAllPlanets());
     }
 
+
+    @Operation(summary = "Busca um planeta por nome ou id", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Encontrado"),
+            @ApiResponse(responseCode = "404", description = "Não Encontrado")
+    })
     @GetMapping()
     public ResponseEntity<PlanetResponse> searchPlanetByParam(@RequestParam(name = "name",required = false) String name,
                                                               @RequestParam(name = "id", required = false)Integer id) throws Exception {
@@ -35,6 +52,12 @@ public class SWPController {
         else return ResponseEntity.ok(service.findPlanetByName(name));
     }
 
+
+    @Operation(summary = "Deleta um planeta por id", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Deletado"),
+            @ApiResponse(responseCode = "400", description = "Não Deletado")
+    })
     @DeleteMapping()
     public ResponseEntity deletePlanet(@RequestParam(name = "id") Integer id){
         service.deletePlanet(id);
